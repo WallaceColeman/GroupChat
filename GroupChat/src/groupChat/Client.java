a0483324301a746e5ac96d916c3560a4287e43f4
Wallace Coleman <wallardcoleman@gmail.com>
2019-03-15 12:43:31 -0400

    Added communication between Client and SeverSide

--- GroupChat/src/groupChat/Client.java
+++ GroupChat/src/groupChat/Client.java
@@ -1,10 +1,33 @@
 package groupChat;
 
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			Socket sock = new Socket("127.0.0.1", 7000);
+			
+			//create an output stream to send data to the server
+			DataOutputStream data2server = new DataOutputStream(sock.getOutputStream());
+			//create an input stream to receive data from server
+			DataInputStream resultFromServer = new DataInputStream(sock.getInputStream());
+			
+			Scanner scan = new Scanner(System.in);
+			
+			while(true) {
+				data2server.writeInt(scan.nextInt());
+				System.out.println(resultFromServer.readInt());
+			}
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- GroupChat/src/groupChat/SeverSide.java
+++ GroupChat/src/groupChat/SeverSide.java
@@ -1,10 +1,28 @@
 package groupChat;
 
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
 public class SeverSide {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			ServerSocket servSock = new ServerSocket(7000);
+			System.out.println("Server started at "+ new Date() + '\n');
+			Socket client = servSock.accept();
+			System.out.println("Initiating connection");
+			DataInputStream inputFromClient = new DataInputStream(client.getInputStream());
+			DataOutputStream output2client = new DataOutputStream(client.getOutputStream());
+			while(true) {
+				int num = inputFromClient.readInt();
+				output2client.writeInt(num*2);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
