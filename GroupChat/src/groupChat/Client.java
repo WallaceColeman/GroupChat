d611e705bdfd523036a3a18de441b12c5875cf22
Wallace Coleman <wallardcoleman@gmail.com>
2019-03-20 14:57:04 -0400

    Group chat now lets multiple members join and send messages to the server

--- GroupChat/src/groupChat/Client.java
+++ GroupChat/src/groupChat/Client.java
@@ -13,17 +13,22 @@ public class Client {
 			//create an output stream to send data to the server
 			DataOutputStream data2server = new DataOutputStream(sock.getOutputStream());
 			//create an input stream to receive data from server
-			DataInputStream resultFromServer = new DataInputStream(sock.getInputStream());
+			DataInputStream fromServer = new DataInputStream(sock.getInputStream());
 			
 			Scanner scan = new Scanner(System.in);
 			System.out.print("Enter name: ");
 			String name = scan.nextLine();
 			
-			while(true) {
-				System.out.println("Enter message: ");
-				data2server.writeInt(scan.nextInt());
-				System.out.println(name + ": " + resultFromServer.readInt());
-			}
+			data2server.writeUTF(name);
+			Send send = new Send(data2server, name);
+			send.start();
+			//fromServer.readInt();
+			
+//			while(true) {
+//				//System.out.println("Enter message: ");
+//				//data2server.writeInt(scan.nextInt());
+//				System.out.println(name + ": " + fromServer.readInt());
+//			}
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			//e.printStackTrace();
@@ -42,5 +47,31 @@ class Recieve extends Thread{
 }
 
 class Send extends Thread{
+	Scanner scan;
+	DataOutputStream data2server;
+	String name;
+	public Send(DataOutputStream data2server, String name) {
+		scan = new Scanner(System.in);
+		this.data2server = data2server;
+		this.name = name;
+	}
+	
+	public void run() {
+		String input;
+		System.out.println("Instructions go here");
+		while(true) {
+			
+//			switch(input.split(" ")[0]) {
+//			
+//			}
+			try {
+				System.out.println("Enter message: ");
+				input = scan.nextLine();
+				data2server.writeUTF(name+": "+input);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
 }
